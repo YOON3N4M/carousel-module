91eae4ab02919ed9051bd2fff4c9685a4d929df0
yoon3n4m <lgmysn2@gmail.com>
2023-06-02 16:21:54 +0900

    setState를 쓰지 않고 해결하기

--- src/components/NewCarousel.js
+++ src/components/NewCarousel.js
@@ -11,26 +11,35 @@ export default class NewCarousel extends Component {
     const colorArr = ["green", "red", "blue", "orange", "purple"];
     let index = this.state;
     return `
-    <ul id="slide"  style="left:${index}px">
+    <ul style="left:${this.state}" id="slide">
     ${numArr.map((n, i) => `<li><div id=${colorArr[i]}></div></li>`).join("")}
     </ul>
     <button id="left"><</button>
     <button id="right">></button>
+    <h1>${this.state}</h1>
     `;
   }
 
   setEvent() {
-    let index = this.state;
     let maxIndex = this.arr.length - 1;
     const boxWidth = 300;
-    this.target.querySelector("#right").addEventListener("click", () => {
-      if (index !== -maxIndex * boxWidth) {
-        this.setState(index - boxWidth);
+
+    this.target.querySelector("#left").addEventListener("click", () => {
+      if (this.state !== 0) {
+        console.log(this.state);
+        this.state = this.state + boxWidth;
+        this.target.querySelector("#slide").style.left = `${this.state}px`;
       }
     });
-    this.target.querySelector("#left").addEventListener("click", () => {
-      if (index !== 0) {
-        this.setState(index + boxWidth);
+
+    this.target.querySelector("#right").addEventListener("click", () => {
+      if (this.state !== -maxIndex * boxWidth) {
+        console.log(this.state);
+        this.state = this.state - boxWidth;
+        this.target.querySelector("#slide").style.left = `${this.state}px`;
+      } else if (this.state === -maxIndex * boxWidth) {
+        this.state = 0;
+        this.target.querySelector("#slide").style.left = "0px";
       }
     });
   }
