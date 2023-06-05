2f163568d1a69f05a35f8f1e8467ed98cb21087f
yoon3n4m <lgmysn2@gmail.com>
2023-06-05 10:46:54 +0900

    일부 네이밍 수정 스타일 적용을 위한 네이밍은 케밥 케이스 스트립트는 카멜 케이스로 수정 했습니다. state등 일부 데이터의 네이밍은 core/Component를 제거하며 수정 예정

--- src/components/NewCarousel.js
+++ src/components/NewCarousel.js
@@ -13,38 +13,41 @@ export default class NewCarousel extends Component {
       .map((n, i) => `<li><div class=${this.colorArr[i]}></div></li>`)
       .join("")}
     </ul>
-    <button class="increaseIndex"><</button>
-    <button class="decreaseIndex">></button>
+    <button data-direction="next" class="arrow"><</button>
+    <button  data-direction="prev" class="arrow">></button>
     <h1>${this.state}</h1>
     `;
   }
 
   setEvent() {
-    const colorQty = this.colorArr.length - 1;
+    const lastIndex = this.colorArr.length - 1;
     const boxWidth = 300;
     // 좌로 이동
-    this.target
-      .querySelector(".increaseIndex")
-      .addEventListener("click", () => {
-        if (this.state !== 0) {
-          this.state = this.state + boxWidth;
-          this.target.querySelector(
-            ".box-wrapper"
-          ).style.transform = `translateX(${this.state}px)`;
-        }
-      });
+    this.target.querySelector(".nextIndex").addEventListener("click", () => {
+      if (this.state !== 0) {
+        this.state = this.state + boxWidth;
+        this.target.querySelector(
+          ".box-wrapper"
+        ).style.transform = `translateX(${this.state}px)`;
+      } else if (this.state === 0) {
+        this.state = -lastIndex * boxWidth;
+        this.target.querySelector(
+          ".box-wrapper"
+        ).style.transform = `translateX(${-lastIndex * boxWidth}px)`;
+      }
+    });
     // 우로 이동
     this.target
       .querySelector(".decreaseIndex")
       .addEventListener("click", () => {
-        if (this.state !== -colorQty * boxWidth) {
+        if (this.state !== -lastIndex * boxWidth) {
           this.state = this.state - boxWidth;
           this.target.querySelector(
             ".box-wrapper"
           ).style.transform = `translateX(${this.state}px)`;
 
           //가장 우측 컨텐츠가 보인 후 첫번째 컨텐츠로 이동
-        } else if (this.state === -colorQty * boxWidth) {
+        } else if (this.state === -lastIndex * boxWidth) {
           this.state = 0;
           this.target.querySelector(".box-wrapper").style.transform =
             "translateX(0px)";
