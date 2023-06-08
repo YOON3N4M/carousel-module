62da00da00d5f48f0c5d349c61a7203844a6da40
yoon3n4m <lgmysn2@gmail.com>
2023-06-08 11:17:34 +0900

    feat: 인디케이터의 dot 클릭시 해당 index로 슬라이딩 기능 구현했습니다.
    carouselWrapper가 함수의 인자로 사용되는 등
    사용빈도가 높기에 인스턴스 변수로 선언 뒤 활용하는 것으로 변했습니다.
    
    * onDotClick으로 인디케이터 dot 클릭시 슬라이딩 되도록 구현했습니다.

--- src/components/NewCarousel.js
+++ src/components/NewCarousel.js
@@ -7,19 +7,22 @@ export default class NewCarousel {
       this.slidingdX = 0;
       this.activeIndex = 0;
       this.lastIndex = this.itemArr.length - 1;
+      this.carouselWrapper;
       this.init();
    }
 
    init() {
       this.target.style.maxWidth = this.width;
+
       this.render();
       this.template();
-      this.controlIndicator(0);
+      this.controlIndicator();
    }
 
    render() {
       this.target.innerHTML = this.template();
       this.addEvent();
+      this.carouselWrapper = this.target.querySelector(".carousel-wrapper");
    }
 
    template() {
@@ -37,12 +40,12 @@ export default class NewCarousel {
             </li>
             `,
          )
-         .join('')}
+         .join("")}
       </ul>
       <div class="navigation">
         <button data-direction="prev" class="prev arrow"><</button>
         <ul class="dot-indicator">
-          ${this.itemArr.map(() => `<button class="dot"></button>`).join('')}
+          ${this.itemArr.map((n, i) => `<button data-index=${i} class="dot"></button>`).join("")}
         </ul>
         <button data-direction="next" class="next arrow">></button>
       </div>
@@ -50,51 +53,60 @@ export default class NewCarousel {
    }
 
    addEvent() {
-      const carouselWrapper = this.target.querySelector('.carousel-wrapper');
-      const prevBtn = this.target.querySelector('.prev');
-      const nextBtn = this.target.querySelector('.next');
+      const prevBtn = this.target.querySelector(".prev");
+      const nextBtn = this.target.querySelector(".next");
+      const dotBtnList = this.target.querySelectorAll(".dot");
 
-      prevBtn.addEventListener('click', () => {
-         this.prev(carouselWrapper);
+      prevBtn.addEventListener("click", () => {
+         this.prev();
       });
-      nextBtn.addEventListener('click', () => {
-         this.next(carouselWrapper);
+      nextBtn.addEventListener("click", () => {
+         this.next();
       });
+      for (var i = 0; i < dotBtnList.length; i++) {
+         dotBtnList[i].addEventListener("click", event => this.onDotClick(event));
+      }
+   }
+
+   next() {
+      if (this.activeIndex < this.lastIndex) {
+         this.activeIndex++;
+         this.sliding(-100 * this.activeIndex);
+      } else if (this.activeIndex === this.lastIndex) {
+         this.activeIndex = 0;
+         this.sliding(0);
+      }
+   }
+
+   prev() {
+      if (this.activeIndex > 0) {
+         this.activeIndex--;
+         this.sliding(-this.activeIndex * 100);
+      } else if (this.activeIndex === 0) {
+         this.activeIndex = this.lastIndex;
+         this.sliding(-this.lastIndex * 100);
+      }
+   }
+
+   onDotClick(event) {
+      const direction = event.target.dataset.index;
+      this.activeIndex = direction;
+      this.sliding(-direction * 100);
+   }
+
+   sliding(figure) {
+      this.carouselWrapper.style.transform = `translateX(${figure}%)`;
+      this.controlIndicator();
    }
 
    controlIndicator() {
-      const dotIndicator = this.target.querySelector('.dot-indicator');
+      const dotIndicator = this.target.querySelector(".dot-indicator");
       const dotArr = dotIndicator.children;
-      const dotToDelete = this.target.querySelector('.active');
+      const dotToDelete = this.target.querySelector(".active");
 
       if (dotToDelete !== null) {
-         dotToDelete.className = 'dot';
+         dotToDelete.className = "dot";
       }
-      dotArr[this.activeIndex].className += ' active';
-   }
-
-   next(carouselWrapper) {
-      if (this.activeIndex < this.lastIndex) {
-         this.activeIndex++;
-         this.sliding(carouselWrapper, -100 * this.activeIndex);
-      } else if (this.activeIndex === this.lastIndex) {
-         this.activeIndex = 0;
-         this.sliding(carouselWrapper, 0);
-      }
-   }
-
-   prev(carouselWrapper) {
-      if (this.activeIndex > 0) {
-         this.activeIndex--;
-         this.sliding(carouselWrapper, -this.activeIndex * 100);
-      } else if (this.activeIndex === 0) {
-         this.activeIndex = this.lastIndex;
-         this.sliding(carouselWrapper, -this.lastIndex * 100);
-      }
-   }
-
-   sliding(carouselWrapper, figure) {
-      carouselWrapper.style.transform = `translateX(${figure}%)`;
-      this.controlIndicator();
+      dotArr[this.activeIndex].className += " active";
    }
 }
