ee0aebe692b487241f469247bb9c0b3f41758d7e
yoon3n4m <lgmysn2@gmail.com>
2023-06-16 10:46:35 +0900

    refator: 네이밍 리팩토링, deviceOption 부분 수정

--- src/components/NewCarousel.js
+++ src/components/NewCarousel.js
@@ -3,28 +3,29 @@ export default class NewCarousel {
       this.target = target;
       this.option = option;
       this.dataArr = this.option.data;
-      this.width = parseInt(this.option.width);
+      this.containerWidth = parseInt(this.option.width);
       this.isResponsive = this.option.isResponsive;
       this.slidesPerView;
       this.slidesPerGroup;
       this.activeIndex = 0;
       this.lastIndex = this.dataArr.length - 1;
-      this.carouselWrapper;
+      this.slideWrapper;
       this.carouselContainer;
       this.startX;
       this.endX;
       this.currentIsMobile;
       this.isMobile;
-      this.target.style.maxWidth = this.width;
+      this.target.style.maxWidth = this.containerWidth;
       this.currentWidth = window.outerWidth;
-      this.itemWidth;
-      this.dotQtyArr;
+      this.slideWidth;
+      this.dotArr;
       this.deviceInit();
       this.setLastIndex();
       this.init();
    }
 
    init() {
+      this.activeIndex = 0;
       this.template();
       this.render();
       this.controlIndicator();
@@ -32,49 +33,43 @@ export default class NewCarousel {
 
    render() {
       this.target.innerHTML = this.template();
-      this.carouselWrapper = this.target.querySelector(".carousel-wrapper");
+      this.slideWrapper = this.target.querySelector(".slide-wrapper");
       this.carouselContainer = this.target.querySelector(".carousel-container");
-      this.carouselWrapper.style.width = `${this.width}vw`;
-      this.target.style.width = `${this.width}vw`;
+      this.slideWrapper.style.width = `${this.containerWidth}vw`;
+      this.target.style.width = `${this.containerWidth}vw`;
       this.addEvent();
    }
 
    template() {
-      //dotQtyArr는 indicator의 인덱스를 위해 선언후 사용
-
-      /* 
-      여기서 this.activeIndex를 0으로 초기화 시켜주지 않으면 Mobile 버전에서 
-      최대 인덱스까지 옮긴 후 PC버전으로 변경 됐을때 PC버전엔 없는 index라서 에러가 남
-      때문에 Mobile, PC 간 전환이 이루어지면 this.activeIndex를 초기화
-      */
-
-      this.activeIndex = 0;
-      this.itemWidth = this.width / this.slidesPerView;
+      this.slideWidth = this.containerWidth / this.slidesPerView;
 
       /*현재 사용하지 않는 메소드 주석처리
       this.reconstructionTemplate();*/
 
-      return `
-      <ul style="transform: translateX(${this.activeIndex}px)" class="carousel-wrapper">
-      ${this.dataArr
+      const dotButtons = this.dotArr.map((n, i) => `<button data-index=${i} class="dot"></button>`).join("");
+      const slideItems = this.dataArr
          .map(
-            (item, index) =>
+            item =>
                `
-            <li>
-              <div>
-                <span class="description">${item.description}</span>
-                <img style="width: ${this.itemWidth}vw" class="item" src=${item.URL}/>
-              </div>
-            </li>
-            `,
+         <li>
+           <div>
+             <span class="description">${item.description}</span>
+             <img style="width: ${this.slideWidth}vw" class="item" src=${item.URL}/>
+           </div>
+         </li>
+         `,
          )
-         .join("")}
+         .join("");
+
+      return `
+      <ul style="transform: translateX(${this.activeIndex}px)" class="slide-wrapper">
+      ${slideItems}
       </ul>
      
       <div class="navigation">
         <button data-direction="prev" class="prev arrow"><</button>
         <ul class="dot-indicator">
-          ${this.dotQtyArr.map((n, i) => `<button data-index=${i} class="dot"></button>`).join("")}
+         ${dotButtons}
         </ul>
         <button data-direction="next" class="next arrow">></button>
       </div>
@@ -96,7 +91,7 @@ export default class NewCarousel {
       }
 
       //dot 갯수
-      this.dotQtyArr = this.dataArr.slice(0, this.lastIndex + 1);
+      this.dotArr = this.dataArr.slice(0, this.lastIndex + 1);
    }
 
    addEvent() {
@@ -111,8 +106,8 @@ export default class NewCarousel {
          dotBtnList[i].addEventListener("click", event => this.onDotClick(event));
       }
       //스와이프 기능
-      this.carouselWrapper.addEventListener("touchstart", event => this.touchStart(event));
-      this.carouselWrapper.addEventListener("touchend", event => this.touchEnd(event));
+      this.slideWrapper.addEventListener("touchstart", event => this.touchStart(event));
+      this.slideWrapper.addEventListener("touchend", event => this.touchEnd(event));
       //반응형 (PC-mobile 캐러셀 전환)
       window.addEventListener("resize", () => this.onResizing());
    }
@@ -139,10 +134,9 @@ export default class NewCarousel {
 
    sliding() {
       let slidingX;
-      const emptyImages = this.calculateIsEmpty();
+      const emptyImages = this.calculateEmptyImages();
 
       slidingX = (100 / this.slidesPerView) * this.slidesPerGroup;
-      //아마 여백을 줄이는 로직은 아래와 같을듯. 임시작성
 
       if (this.slidesPerGroup > 1 && emptyImages > 0) {
          if (this.activeIndex === this.lastIndex) {
@@ -154,15 +148,15 @@ export default class NewCarousel {
          }
       }
 
-      this.carouselWrapper.style.transform = `translateX(${-slidingX * this.activeIndex}%)`;
+      this.slideWrapper.style.transform = `translateX(${-slidingX * this.activeIndex}%)`;
       this.controlIndicator();
    }
 
-   calculateIsEmpty() {
+   calculateEmptyImages() {
       const x = this.dataArr.length;
       const y = this.slidesPerView;
       const z = this.slidesPerGroup;
-      //마지막 인덱스에서 비는 이미지의 개수 = n, 0이면 비지 않는 완전한 이미지
+      //마지막 인덱스에서 비는 이미지의 개수 = n, 0이면 빈 이미지가 없는 완전한 슬라이드 뷰
       const n = (y - (x % z)) % z;
 
       return n;
@@ -208,14 +202,12 @@ export default class NewCarousel {
          //pc
          this.currentIsMobile = false;
          this.isMobile = false;
-         this.slidesPerGroup = this.option.deviceOption.pcSlidesPerGroup;
-         this.slidesPerView = this.option.deviceOption.pcSlidesPerView;
+         this.setDeviceOption();
       } else if (this.currentWidth < 769 && this.currentIsMobile === undefined) {
          //mobile
          this.currentIsMobile = true;
          this.isMobile = true;
-         this.slidesPerGroup = this.option.deviceOption.mobileSlidesPerGroup;
-         this.slidesPerView = this.option.deviceOption.mobileSlidesPerView;
+         this.setDeviceOption();
       }
    }
 
@@ -229,52 +221,60 @@ export default class NewCarousel {
          //mobile
          this.isMobile = true;
       }
-      this.changeDevice();
-   }
 
-   changeDevice() {
       if (this.currentIsMobile !== this.isMobile) {
-         if (this.isMobile) {
-            this.currentIsMobile = true;
-            this.slidesPerGroup = this.option.deviceOption.mobileSlidesPerGroup;
-            this.slidesPerView = this.option.deviceOption.mobileSlidesPerView;
-            this.init();
-         } else {
-            this.currentIsMobile = false;
-            this.slidesPerGroup = this.option.deviceOption.pcSlidesPerGroup;
-            this.slidesPerView = this.option.deviceOption.pcSlidesPerView;
-            this.init();
-         }
+         this.changeDevice();
       }
    }
 
+   changeDevice() {
+      if (this.isMobile) {
+         this.currentIsMobile = true;
+         this.setDeviceOption();
+         this.init();
+      } else {
+         this.currentIsMobile = false;
+         this.setDeviceOption();
+         this.init();
+      }
+   }
+
+   setDeviceOption() {
+      if (this.isMobile) {
+         this.slidesPerGroup = this.option.deviceOption.mobileSlidesPerGroup;
+         this.slidesPerView = this.option.deviceOption.mobileSlidesPerView;
+      } else {
+         this.slidesPerGroup = this.option.deviceOption.pcSlidesPerGroup;
+         this.slidesPerView = this.option.deviceOption.pcSlidesPerView;
+      }
+   }
    /* 브라우저 리사이징으로 기기 전환이 감지 되면 그에 맞는 템플릿으로 재구성
    reconstructionTemplate() {
       if (this.isResponsive && this.isMobile === false) {
          //PC 버전
          //슬라이드의 크기
-         this.itemWidth = this.width / this.partition;
+         this.slideWidth = this.containerWidth / this.partition;
          //인덱스
          const dotQty = Math.ceil(this.dataArr.length / this.partition);
-         this.dotQtyArr = this.dataArr.slice(0, dotQty);
+         this.dotArr = this.dataArr.slice(0, dotQty);
          //if PC 버전의 슬라이드 갯수를 지정했다면
          if (this.pcSlideQty !== undefined) {
             this.dataArr = this.dataArr.slice(0, this.pcSlideQty);
          }
          //파티션의 갯수가 2이상일때 계산된 숫자(dotQty)로 lastIndex 설정
          if (this.partition > 1) {
-            this.lastIndex = this.dotQtyArr.length - 1;
+            this.lastIndex = this.dotArr.length - 1;
          } else {
             this.lastIndex = this.dataArr.length - 1;
          }
       } else {
          //Mobile 버전
-         this.itemWidth = this.width;
+         this.slideWidth = this.containerWidth;
          //if Mobile 버전의 슬라이드 갯수를 지정했다면
          if (this.mobileSlideQty !== undefined) {
             this.dataArr = this.dataArr.slice(0, this.mobileSlideQty);
          }
-         this.dotQtyArr = this.dataArr;
+         this.dotArr = this.dataArr;
          this.lastIndex = this.dataArr.length - 1;
       }
    }
