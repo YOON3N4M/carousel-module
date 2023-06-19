f658a4a7f32c4fab0e3e8ec0af0082832c7ffe7b
yoon3n4m <lgmysn2@gmail.com>
2023-06-19 09:22:41 +0900

    docs: 테스트 캐러셀 하나만을 남겨두고 나머지 캐러셀 실행문, 옵션 제거

--- src/app.js
+++ src/app.js
@@ -5,52 +5,15 @@ const test = { a: 123 };
 
 class App {
    constructor() {
-      const hyundaiCarousel = document.querySelector(".hyundai");
-      const palisadeCarousel = document.querySelector(".palisade");
-      const thirdCarousel = document.querySelector(".third");
-      const lastCarousel = document.querySelector(".last");
       const testCarousel = document.querySelector(".test");
-
-      const hyundaiOption = {
-         data: third,
-         width: thirdCarousel.dataset.width,
-         isResponsive: true,
-         pcPartition: 2,
-         qtyToSlidePc: 2,
-         mobilePartition: 1,
-         qtyToSlideMobile: 1,
-      };
-
-      const palisadeOption = {
-         data: third,
-         width: thirdCarousel.dataset.width,
-         isResponsive: true,
-         pcPartition: 5,
-         mobilePartition: 3,
-         qtyToSlide: 1,
-      };
-
-      const lastOption = {
-         data: last,
-         width: lastCarousel.dataset.width,
-         isResponsive: true,
-         pcPartition: 5,
-         qtyToSlidePc: 1,
-         mobilePartition: 4,
-         qtyToSlideMobile: 2,
-      };
 
       const testOption = {
          data: last,
          width: testCarousel.dataset.width,
          isResponsive: true,
-         deviceOption: { pcSlidesPerView: 4, pcSlidesPerGroup: 2, mobileSlidesPerView: 5, mobileSlidesPerGroup: 2 },
+         deviceOption: { pcSlidesPerView: 3, pcSlidesPerGroup: 2, mobileSlidesPerView: 5, mobileSlidesPerGroup: 2 },
       };
-      /*NEwCarousel(타겟, 데이터, html의width값, 반응형 분할 여부, 파티션 갯수,pc 슬라이드 갯수(optional),모바일 슬라이드 갯수(optional))*/
-      // new NewCarousel(hyundaiCarousel, hyundaiOption);
-      //new NewCarousel(palisadeCarousel, palisade, palisadeCarousel.dataset.width, false, 1);
 
-      // new NewCarousel(lastCarousel, lastOption);
       new NewCarousel(testCarousel, testOption);
    }
 }
