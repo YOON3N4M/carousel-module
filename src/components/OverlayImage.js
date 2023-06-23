48cdaacf94a61b5f38b39945d3420f88c2fc0bc0
yoon3n4m <lgmysn2@gmail.com>
2023-06-23 13:41:58 +0900

    feat: 이벤트 쓰로틀링 적용, 핸들 중심점 맞추는 부분 자바스크립트로 자동화

--- src/components/OverlayImage.js
+++ src/components/OverlayImage.js
@@ -2,6 +2,7 @@ export default class OverlayImage {
    constructor(target) {
       this.target = target;
       this.handle = document.querySelector(".handle");
+      this.handleWidth = this.handle.offsetWidth;
       this.leftBox = document.querySelector(".left-box");
       this.rightBox = document.querySelector(".right-box");
       this.isPress = false;
@@ -9,6 +10,7 @@ export default class OverlayImage {
       this.leftBoxWidth = 50;
       this.rightBoxWidth = 100 - this.leftBoxWidth;
       this.initialHandleOffsetLeft;
+      this.timer;
       this.init();
    }
 
@@ -17,6 +19,12 @@ export default class OverlayImage {
       this.leftBox.style.width = `${this.leftBoxWidth}%`;
       this.rightBox.style.width = `${this.rightBoxWidth}%`;
       this.initialHandleOffsetLeft = this.handle.offsetLeft;
+      /* 
+      transform이 아닌 margin값을 음수로 핸들 위치를 조정하면 이벤트에 에러가 생김
+      해당 값은 핸들 요소의 width 값 반을 음수로 자동으로 할당 되도록 변경
+      ex) 핸들 50px => translateX(-25px)
+      */
+      this.handle.style.transform = `translateX(-${this.handleWidth / 2}px)`;
    }
 
    addEvent() {
@@ -26,7 +34,7 @@ export default class OverlayImage {
    }
 
    start(e) {
-      console.log("누름");
+      console.log(this);
       console.log(this.initialHandleOffsetLeft);
       this.prevPosX = e.clientX;
       this.isPress = true;
@@ -34,10 +42,15 @@ export default class OverlayImage {
 
    onHandleMove(e) {
       if (this.isPress) {
-         const posX = this.prevPosX - e.clientX;
-         this.prevPosX = e.clientX;
-         this.handle.style.left = this.handle.offsetLeft - posX + "px";
-         this.changeBoxWidth();
+         if (!this.timer) {
+            this.timer = setTimeout(() => {
+               this.timer = null;
+               const posX = this.prevPosX - e.clientX;
+               this.prevPosX = e.clientX;
+               this.handle.style.left = this.handle.offsetLeft - posX + "px";
+               this.changeBoxWidth();
+            }, 10);
+         }
       }
    }
 
