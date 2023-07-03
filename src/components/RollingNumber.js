dc922db312d1fa5c11d29d628adabe18689e3069
yoon3n4m <lgmysn2@gmail.com>
2023-07-03 15:48:10 +0900

    feat: 머릿말, 꼬릿말 추가

--- src/components/RollingNumber.js
+++ src/components/RollingNumber.js
@@ -1,7 +1,7 @@
 export default class RollingNumber {
    constructor(target, number) {
       this.container = document.querySelector(".rolling-number-container");
-      this.targetArr = [...document.querySelectorAll('div[data-animation="rollingNumber"]')];
+      this.targetArr = [...document.querySelectorAll('[data-animation="rollingNumber"]')];
       //쉼표 적용을 위해 문자열로 변환
       this.isActive = false;
       this.spanHeight;
@@ -18,7 +18,7 @@ export default class RollingNumber {
       this.targetArr.forEach(target => (target.innerHTML = `<span class="num">0</span>`));
       const initialNum = document.querySelector(".num");
 
-      this.targetArr.forEach(t => (t.style.width = `${t.dataset.value.length * initialNum.offsetWidth}px`));
+      // this.targetArr.forEach(t => (t.style.width = `${t.dataset.value.length * initialNum.offsetWidth}px`));
       this.targetArr.forEach(t => (t.style.height = `${initialNum.offsetHeight}px`));
       console.log(initialNum.offsetHeight);
       console.log(this.targetArr[0].dataset.value.length);
@@ -26,7 +26,9 @@ export default class RollingNumber {
 
    whenViewportInit() {
       this.targetArr.forEach(target => (target.innerHTML = ""));
+      this.targetArr.forEach(el => (el.innerHTML += `<span>${el.dataset.headtext}</span>`));
       this.targetArr.forEach(el => this.setSlide(el));
+      this.targetArr.forEach(el => (el.innerHTML += `<span>${el.dataset.tailtext}</span>`));
       const numSpan = document.querySelector(".num");
       this.spanHeight = numSpan.offsetHeight;
    }
@@ -74,7 +76,6 @@ export default class RollingNumber {
 
          //  const wrapperEl = el.querySelector(`.22233`);
          // console.log(wrapperEl);
-
          /* 
          spanElement.innerHTML += `<span class="num ${classId}" data-text="${text}">
          ${numSpaceArr.map(i => `<span class="num-list">${i}</span>`)}
@@ -84,7 +85,7 @@ export default class RollingNumber {
 
          setTimeout(() => {
             this.animate(item);
-         }, this.delay * i);
+         }, 0);
       }, 100);
    }
 
