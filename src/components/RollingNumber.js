91b0a6fe32eb80ee3281f011ff2a54fec41a79e5
yoon3n4m <lgmysn2@gmail.com>
2023-07-11 14:36:38 +0900

    refactor: 작동방식 개선, 불필요한 코드 정리
    기기변경시 템플릿을 다시 그리던 방식에서 애니메이션,css 만 초기화 하는
    방식으로 변경

--- src/components/RollingNumber.js
+++ src/components/RollingNumber.js
@@ -2,20 +2,20 @@ export default class RollingNumber {
    constructor(el) {
       this.el = el;
       this.spanHeight;
-      this.isMobile;
-      this.currentIsMobile;
+      this.isMobile = null;
       this.slideOptionAboutIndex = [2, 5, 10, 15, 22, 25];
       this.init();
+      this.observer;
+      this.sliderArr;
       // 이 부분 false 면 숫자가 위로 돌아가고, true면 아래로 돌아감
       this.isToDown = false;
-      this.observer;
    }
 
    init() {
       this.setTemplate();
       this.setObserver();
       this.setDomStyle();
-      window.addEventListener("resize", () => this.onResizing());
+      window.addEventListener("resize", () => this.resizeHandler());
    }
 
    setObserver() {
@@ -36,35 +36,63 @@ export default class RollingNumber {
       const headTextEl = `<span class="roll-head-text">${this.el.dataset.headtext !== undefined ? this.el.dataset.headtext : ""}</span>`;
       const tailTextEl = `<span class="roll-tail-text">${this.el.dataset.tailtext}</span>`;
       this.el.insertAdjacentHTML("afterbegin", tailTextEl);
-      //이 부분에 setSlide
       this.el.insertAdjacentHTML("afterbegin", headTextEl);
-      this.setSlide(this.el);
+      this.setSlide();
+      this.sliderArr = [...document.querySelectorAll(".roll-slide")];
    }
 
-   slideListHTML() {}
+   setSlide() {
+      const valueData = this.el.dataset.value;
+      //쉼표를 적용한 문자열의 배열
+      const replacedValueData = valueData.replace(/\B(?<!\.\d*)(?=(\d{3})+(?!\d))/g, ",").split("");
+
+      replacedValueData.forEach((item, idx) => {
+         const isDot = item == "," || item == ".";
+         const intItem = parseInt(item);
+         const randomNumArr = [];
+
+         for (var i = 0; i <= this.slideOptionAboutIndex[idx]; i++) {
+            let randomNumber = Math.floor(Math.random() * 10);
+            randomNumArr.push(randomNumber);
+         }
+
+         this.isToDown ? randomNumArr.unshift(intItem) : randomNumArr.push(intItem);
+         // prettier-ignore
+         const slideContent = isDot
+            ? `<span class="roll-dot">${item}</span>`
+            : `<div data-value=${item} data-index=${idx} class="roll-slide">
+                  ${randomNumArr.map((number) =>
+                      `<span class="roll-num">${number}</span>`).join("")
+                     }
+               </div>`;
+
+         //머릿말은 없어도 꼬릿말은 고정적으로 있기에 슬라이드는 꼬릿말의 앞쪽에 dom 생성
+         this.el.lastChild.insertAdjacentHTML("beforebegin", `${slideContent}`);
+      });
+   }
+
    setDomStyle() {
       const initialSpan = document.querySelector(".roll-tail-text");
-      //해당 부분도 따로 분리 예정
-      this.el.style.height = `${initialSpan.offsetHeight}px`;
-      this.el.style.minWidth = `${initialSpan.offsetWidth * this.el.dataset.value.length}px`;
       this.spanHeight = initialSpan.offsetHeight;
+      this.el.style.height = `${this.spanHeight}px`;
+      //.el.style.minWidth = `${this.spanWidth * this.el.dataset.value.length}px`;
    }
 
-   onResizing() {
-      const mediaQueryWidth = window.innerWidth;
+   resizeHandler() {
+      const crrIsMobile = window.innerWidth <= 769;
 
-      //리사이징 변경 감지
-      if (mediaQueryWidth >= 769) {
-         //pc
-         this.isMobile = false;
-      } else if (mediaQueryWidth < 769) {
-         //mobile
-         this.isMobile = true;
+      if (crrIsMobile !== this.isMobile) {
+         this.isMobile = crrIsMobile;
+         this.whenChangeDevice();
       }
+   }
 
-      if (this.currentIsMobile !== this.isMobile) {
-         this.changeDevice();
-      }
+   whenChangeDevice() {
+      this.setDomStyle();
+      this.sliderArr.forEach(slide => (slide.style.transition = `transform 0s ease`));
+      this.sliderArr.forEach(slide => (slide.style.transform = `translateY(${0}px)`));
+      // slideArr.forEach(slide => (slide.style.transition = `transform 2.5s ease`));
+      this.setObserver();
    }
 
    whenViewportInit() {
@@ -75,47 +103,14 @@ export default class RollingNumber {
       this.observer.disconnect();
    }
 
-   setSlide(el) {
-      const valueData = el.dataset.value;
-      //쉼표를 적용한 문자열의 배열
-      console.log(parseInt(valueData));
-      const replacedValueData = valueData.replace(/\B(?<!\.\d*)(?=(\d{3})+(?!\d))/g, ",").split("");
-
-      replacedValueData.forEach((item, idx) => {
-         const isDot = item == "," || item == ".";
-         const intItem = parseInt(item);
-
-         let numArr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
-         let randomNumArr = [];
-
-         for (var i = 0; i <= this.slideOptionAboutIndex[idx]; i++) {
-            let randomNumber = numArr[Math.floor(Math.random() * numArr.length)];
-            randomNumArr.push(randomNumber);
-         }
-
-         this.isToDown ? randomNumArr.unshift(intItem) : randomNumArr.push(intItem);
-
-         const slideContent = isDot
-            ? `<span class="roll-dot">${item}</span>`
-            : `<div data-value=${item} data-index=${idx} class="roll-slide">
-                  ${randomNumArr.map(number => `<span class="roll-num">${number}</span>`).join("")}
-               </div>`;
-
-         //머릿말은 없어도 꼬릿말은 고정적으로 있기에 슬라이드는 꼬릿말의 앞쪽에 dom 생성
-         el.lastChild.insertAdjacentHTML("beforebegin", `${slideContent}`);
-      });
-   }
-
    animate() {
-      const slideArr = [...document.querySelectorAll(".roll-slide")];
-
-      slideArr.forEach(slide => {
+      this.sliderArr.forEach(slide => (slide.style.transition = `transform 2.5s ease`));
+      this.sliderArr.forEach(slide => {
          const numberIndex = parseInt(slide.dataset.index);
          const direction = this.isToDown ? `+` : `-`;
-
-         slide.style.marginTop = `${direction}${
-            (this.slideOptionAboutIndex[numberIndex] + 1) * this.spanHeight
-         }px`;
+         slide.style.transform = `translateY(${direction}${
+            ((this.slideOptionAboutIndex[numberIndex] + 1) * this.spanHeight) / 2
+         }px)`;
       });
    }
 }
